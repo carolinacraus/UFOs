3dee25b79bfcbc90d4272682d8ffa2d3c855db5c
carolinacraus <carocraus@gmail.com>
2020-10-18 18:38:36 -0500

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// import data from data.js
+const tableData = data; 
+
+// reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+
+// function to build table 
+function buildTable(data) {
+    tbody.html("")
+}
+
+// F
